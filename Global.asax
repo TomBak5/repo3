82f92e23cea0de8c95ba74c40fb0dbf2d1fe0b39
pukuotas <tbinfo88@gmail.com>
2017-02-24 09:43:18 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BUS_MVC.WebApiApplication" Language="C#" %>
---
+++ funkcijos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace BUS_MVC
+{
+    public class funkcijos
+    {
+
+        public string fjos_to_hash(string pass823)
+        {
+
+
+            System.Security.Cryptography.SHA256 sha256 = System.Security.Cryptography.SHA256.Create();
+
+            var pass_in_bytes = sha256.ComputeHash(Encoding.UTF7.GetBytes(pass823));
+
+            //var pass_hash = Encoding.UTF7.GetString(pass_in_bytes);
+
+            var pass_hash = System.Convert.ToBase64String(pass_in_bytes);
+
+            return pass_hash;
+
+
+        }
+
+
+
+    }
+}
